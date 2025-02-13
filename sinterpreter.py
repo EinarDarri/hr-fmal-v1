8dad319e38e2da722acb0e8435e150aead2884cb
einardarri <git@einardarri.is>
2025-02-13 10:52:56 +0000

    setup

--- sinterpreter.py
+++ sinterpreter.py
@@ -1,13 +1,69 @@
+from collections.abc import Callable
+
 class InvalidOperator(Exception):
 	pass
 
+ITEM_TYPE = int | str
+
 class SInterpreter:
-	__command_stack: list
+	COMMANDS: dict[str, Callable[[str | int | None], None]] = {
+		
+	}
+	__command_stack: list[str]
+	__var_map: dict[str, int]
 
 	def __init__(self) -> None:
 		self.__command_stack = []
+		self.__var_map = {}
 
 	def cycle(self) -> None:
+		pass
+
+	# commands
+	@classmethod
+	def _push(cls, item : ITEM_TYPE) -> None:
+		"""
+		pushes the operand op onto the stack
+		"""
+		pass
+
+	@classmethod
+	def _add(cls) -> None:
+		"""
+		addition: pops the two top elements from the stack, adds their 
+		values and pushes the result back onto the stack
+		"""
+		pass
+
+	@classmethod
+	def _mult(cls) -> None:
+		"""
+		multiplication: pops the two top elements from the stack,  
+		multiplies their values and pushes the result back onto the stack 
+		"""
+		pass
+
+	@classmethod
+	def _uminus(cls) -> None:
+		"""
+		unary minus: pops the top element from stack, changes its sign  
+		and pushes the result back onto the stack
+		"""
+		pass
+
+	@classmethod
+	def _assign(cls) -> None:
+		"""
+		assignment: pops the two top elements from the stack, assigns  
+		the first element (a value) to the second element (a variable) 
+		"""
+		pass
+
+	@classmethod
+	def _print(cls) -> None:
+		"""
+		prints the value currently on top of the stack 
+		"""
 		pass
 
 
